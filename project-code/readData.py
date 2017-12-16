d0daf2422cdafa1aabafb8f9d7acaf58e95709a4
adam <ibrahimadam193@gmail.com>
2017-12-16 04:30:20 -0500

    Wrap data-loading into single script, add metadata
    Loading the data now loads some additional metadata, for code
    documentation purposes.
    
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
          modified:   project-code/readData.py
          deleted:    project-code/readFaceData.py

--- project-code/readData.py
+++ project-code/readData.py
@@ -29,7 +29,7 @@ class batch:
         self.images = images
         self.batchIndexes_ = None
 
-    def nextBatch(self, amount):
+    def next_batch(self, amount):
         if self.batchIndexes_ is None:
             numImages = self.images.shape[0]
             self.batchIndexes_ = np.arange(numImages)
@@ -46,10 +46,78 @@ class batch:
         #print(type(batchImages))
         #print(batchImages.shape)
         self.batchIndexes_ = self.batchIndexes_[amount:]
-        return self.images[batchImages, :]
+        # To keep with interface of mnist data set from tensorflow.
+        return (self.images[batchImages, :], None)
 
 class faces:
     def __init__(self, images, testRatio=.5):
         numTest = int(images.shape[0] * testRatio)
         self.test = batch(images[:numTest])
         self.train = batch(images[numTest:])
+
+def loadMnist():
+    from tensorflow.examples.tutorials.mnist import input_data
+    return input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+def loadFace(path):
+    faceStuff = np.load(path)
+    # Of shape (imageHeight, imageWidth, images)
+    data = faceStuff['face_images']
+    height, weight, images = data.shape
+
+    # I want to reshape to (images, imageHeight*imageWidth)
+    data = np.rollaxis(data, 2, 0)
+    return faces(data.reshape(images, height*weight), testRatio=.3)
+
+dataCharacteristics = {
+    'mnist' : {
+        'name' : 'mnist-digits',
+        'photoResolution' : (28, 28),
+        'numInputs' : 784,
+        'load' : loadMnist
+    },
+    'faces' : {
+        'name' : 'faces',
+        'photoResolution' : (96, 96),
+        'numInputs' : 9216,
+        'path' : "/home/adam/Documents/scratch/faces/face_images.npz"
+    },
+    'smallFaces' : {
+        'name' : 'faces 60x60',
+        'photoResolution' : (60, 60),
+        'numInputs' : 3600,
+        'path' : "/home/adam/Documents/scratch/faces/small_face_images.npz"
+    },
+    'reallySmallFaces' : {
+        'name' : 'faces 28x28',
+        'photoResolution' : (28, 28),
+        'numInputs' : 784,
+        'path' : "/home/adam/Documents/scratch/faces/really_small_face_images.npz"
+    },
+}
+
+dataCharacteristics['faces']['load'] = \
+    lambda: loadFace(dataCharacteristics['faces']['path'])
+dataCharacteristics['smallFaces']['load'] = \
+    lambda: loadFace(dataCharacteristics['smallFaces']['path'])
+dataCharacteristics['reallySmallFaces']['load'] = \
+    lambda: loadFace(dataCharacteristics['reallySmallFaces']['path'])
+
+def dictCopy(toCopy):
+    copy = {}
+    primitives = (str, int, bool, float)
+    sequence = (tuple, list)
+    for key, value in toCopy.items():
+        if isinstance(value, primitives):
+            copy[key] = value
+        elif isinstance(value, sequence):
+            copy[key] = str(value)
+        elif isinstance(value, dict):
+            copy[key] = dictCopy(value)
+    return copy
+
+cleanCharacteristics = dictCopy
+
+def loadData(which):
+    return (dataCharacteristics[which]['load'](),
+            cleanCharacteristics(dataCharacteristics[which]))
